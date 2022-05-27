e73a867dd03bfa32a05b628bd5aafe4a644583fc
TestLeafPages <sarath.muthuswamy@qeagle.com>
2022-05-27 19:30:58 +0530

    Update LearnStatic.java

--- week4/day5/LearnStatic.java
+++ week4/day5/LearnStatic.java
@@ -13,7 +13,7 @@ public class LearnStatic {
 public static void main(String[] args) {
 	LearnStatic emp=new LearnStatic();
 	emp.getEmployeeDetails("Prakash", 1011,"HCL");
-	System.out.println(emp.empName+" "+emp.empId+" "+emp.empCompany);
+	
 	
 	LearnStatic emp2=new LearnStatic();
 	emp2.getEmployeeDetails("Vel", 1012,"Testleaf");
@@ -21,6 +21,7 @@ public static void main(String[] args) {
 	LearnStatic emp3=new LearnStatic();
 	emp3.getEmployeeDetails("RajBabu", 1013,"TCS");
 	
+	System.out.println(emp.empName+" "+emp.empId+" "+emp.empCompany);
 	System.out.println(emp2.empName+" "+emp2.empId+" "+emp2.empCompany);
 	System.out.println(emp3.empName+" "+emp3.empId+" "+emp3.empCompany);
 	
